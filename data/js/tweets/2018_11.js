465d6f192c240a7d8a7e9cf8c5fcdb192d93a2ae
Stefan Seiz <seiz@imd.net>
2018-11-09 08:14:21 +0100

    Added 1 tweets

--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,273 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Thu Nov 08 11:32:34 +0000 2018",
+		"id": 1060495290564796400,
+		"id_str": "1060495290564796416",
+		"full_text": "Ich sach nur: https://t.co/SRHUvxr3i2 https://t.co/tSh9xttg35",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			37
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SRHUvxr3i2",
+					"expanded_url": "http://prangr.com",
+					"display_url": "prangr.com",
+					"indices": [
+						14,
+						37
+					]
+				},
+				{
+					"url": "https://t.co/tSh9xttg35",
+					"expanded_url": "https://twitter.com/ChristophKappes/status/1060492510055841792",
+					"display_url": "twitter.com/ChristophKappe…",
+					"indices": [
+						38,
+						61
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 196,
+			"friends_count": 139,
+			"listed_count": 30,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 646,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15602,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1060492510055841800,
+		"quoted_status_id_str": "1060492510055841792",
+		"quoted_status_permalink": {
+			"url": "https://t.co/tSh9xttg35",
+			"expanded": "https://twitter.com/ChristophKappes/status/1060492510055841792",
+			"display": "twitter.com/ChristophKappe…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Nov 08 11:21:32 +0000 2018",
+			"id": 1060492510055841800,
+			"id_str": "1060492510055841792",
+			"full_text": "Leider wahr. https://t.co/LTDkcovp1Q",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				12
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/LTDkcovp1Q",
+						"expanded_url": "https://twitter.com/martinlindner/status/1060478003908616192",
+						"display_url": "twitter.com/martinlindner/…",
+						"indices": [
+							13,
+							36
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 19961453,
+				"id_str": "19961453",
+				"name": "Christoph Kappes",
+				"screen_name": "ChristophKappes",
+				"location": "Hamburg, Germany",
+				"description": "Follow you & follow me, ahaaa. Ich habe meine Meinung nur, solange ich keine bessere habe. Made with ❤ in Paris.",
+				"url": "https://t.co/gwZFd0Jjrq",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/gwZFd0Jjrq",
+								"expanded_url": "http://www.christophkappes.de",
+								"display_url": "christophkappes.de",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 8148,
+				"friends_count": 2894,
+				"listed_count": 434,
+				"created_at": "Tue Feb 03 09:14:01 +0000 2009",
+				"favourites_count": 37460,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 50241,
+				"lang": "de",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFCC4D",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/959386837243613186/m6Tsu-If_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/959386837243613186/m6Tsu-If_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/19961453/1491749752",
+				"profile_link_color": "FA743E",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "C9CCC8",
+				"profile_text_color": "6B696B",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "4d973f44ad72d282",
+				"url": "https://api.twitter.com/1.1/geo/id/4d973f44ad72d282.json",
+				"place_type": "city",
+				"name": "Valle Gran Rey",
+				"full_name": "Valle Gran Rey, Spain",
+				"country_code": "ES",
+				"country": "Spain",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-17.3489529,
+								28.0778657
+							],
+							[
+								-17.2661795,
+								28.0778657
+							],
+							[
+								-17.2661795,
+								28.1536961
+							],
+							[
+								-17.3489529,
+								28.1536961
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1060478003908616200,
+			"quoted_status_id_str": "1060478003908616192",
+			"quoted_status_permalink": {
+				"url": "https://t.co/LTDkcovp1Q",
+				"expanded": "https://twitter.com/martinlindner/status/1060478003908616192",
+				"display": "twitter.com/martinlindner/…"
+			},
+			"retweet_count": 0,
+			"favorite_count": 8,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "de"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Ich sach nur: https://t.co/SRHUvxr3i2 https://t.co/tSh9xttg35"
+	},
+	{
 		"created_at": "Thu Nov 08 07:06:49 +0000 2018",
 		"id": 1060428409426329600,
 		"id_str": "1060428409426329600",
