b16ae3361bef35e5f61e0bf90c3679c097902cee
Stefan Seiz <seiz@imd.net>
2019-01-18 08:14:21 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 14666,
-	"created_at": "Thu Jan 17 07:14:20 +0000 2019"
+	"tweets": 14667,
+	"created_at": "Fri Jan 18 07:14:20 +0000 2019"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,341 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Thu Jan 17 17:11:08 +0000 2019",
+		"id": 1085947640775360500,
+		"id_str": "1085947640775360512",
+		"full_text": "Was haben die nur eingenommen? https://t.co/bWpfqgkAXt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			30
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bWpfqgkAXt",
+					"expanded_url": "https://twitter.com/UnitymediaHilfe/status/1085946691952525313",
+					"display_url": "twitter.com/UnitymediaHilf…",
+					"indices": [
+						31,
+						54
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 192,
+			"friends_count": 139,
+			"listed_count": 30,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 652,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15688,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1085946691952525300,
+		"quoted_status_id_str": "1085946691952525313",
+		"quoted_status_permalink": {
+			"url": "https://t.co/bWpfqgkAXt",
+			"expanded": "https://twitter.com/UnitymediaHilfe/status/1085946691952525313",
+			"display": "twitter.com/UnitymediaHilf…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Jan 17 17:07:21 +0000 2019",
+			"id": 1085946691952525300,
+			"id_str": "1085946691952525313",
+			"full_text": "Hey ihr geilen Leute da draußen! Eigentlich wollten wir heute wieder bei #UMquickANDdirty auf den Putz hauen➡️klappt aber nicht😿😭 Aber unsere Guides Melissa, Tara und Matze sind zw. 18-19 Uhr mit der Tastatur bewaffnet &amp; schießen nur so Antworten raus🔫🔫🔫 https://t.co/0w0rlJTuVY",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				258
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "UMquickANDdirty",
+						"indices": [
+							73,
+							89
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1085942921331912700,
+						"id_str": "1085942921331912704",
+						"indices": [
+							259,
+							282
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1085942921331912704/pu/img/DTt4Plm-X76-t0oU.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1085942921331912704/pu/img/DTt4Plm-X76-t0oU.jpg",
+						"url": "https://t.co/0w0rlJTuVY",
+						"display_url": "pic.twitter.com/0w0rlJTuVY",
+						"expanded_url": "https://twitter.com/UnitymediaHilfe/status/1085946691952525313/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1085942921331912700,
+						"id_str": "1085942921331912704",
+						"indices": [
+							259,
+							282
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1085942921331912704/pu/img/DTt4Plm-X76-t0oU.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1085942921331912704/pu/img/DTt4Plm-X76-t0oU.jpg",
+						"url": "https://t.co/0w0rlJTuVY",
+						"display_url": "pic.twitter.com/0w0rlJTuVY",
+						"expanded_url": "https://twitter.com/UnitymediaHilfe/status/1085946691952525313/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 29133,
+							"variants": [
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1085942921331912704/pu/vid/320x180/iY1VxkOb4cD1Vkqj.mp4?tag=8"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1085942921331912704/pu/vid/1280x720/4USFudyjJrYFUhKy.mp4?tag=8"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1085942921331912704/pu/pl/qc8O2g1u4v8kUxzM.m3u8?tag=8"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1085942921331912704/pu/vid/640x360/TNeTfNVPFfKChCco.mp4?tag=8"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 19017286,
+				"id_str": "19017286",
+				"name": "Unitymedia Hilfe",
+				"screen_name": "UnitymediaHilfe",
+				"location": "Köln, Deutschland",
+				"description": "Impressum: https://t.co/eiBKTZrB9o\n\nKennst du schon unsere Community?",
+				"url": "https://t.co/92QEeMYYuL",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/92QEeMYYuL",
+								"expanded_url": "https://community.unitymedia.de",
+								"display_url": "community.unitymedia.de",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/eiBKTZrB9o",
+								"expanded_url": "http://www.unitymedia.de/impressum",
+								"display_url": "unitymedia.de/impressum",
+								"indices": [
+									11,
+									34
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 26024,
+				"friends_count": 118,
+				"listed_count": 202,
+				"created_at": "Thu Jan 15 10:14:22 +0000 2009",
+				"favourites_count": 1191,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 92151,
+				"lang": "de",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "B8DBEF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1041662979408912390/sGXhiE0u_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1041662979408912390/sGXhiE0u_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/19017286/1545380481",
+				"profile_link_color": "1E9B41",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "BAD1ED",
+				"profile_text_color": "031041",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "de"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Was haben die nur eingenommen? https://t.co/bWpfqgkAXt"
+	},
+	{
 		"created_at": "Wed Jan 16 20:34:14 +0000 2019",
 		"id": 1085636366136983600,
 		"id_str": "1085636366136983552",
