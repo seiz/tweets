50d9e00d43c7b550d063722b94c045029e1913c8
Stefan Seiz <seiz@imd.net>
2019-04-24 09:14:21 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 14727,
-	"created_at": "Thu Apr 18 07:14:19 +0000 2019"
+	"tweets": 14728,
+	"created_at": "Wed Apr 24 07:14:20 +0000 2019"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Tue Apr 23 07:36:29 +0000 2019",
+		"id": 1120592263564996600,
+		"id_str": "1120592263564996608",
+		"full_text": "Facepalm der Woche: https://t.co/rpBOz0X1i5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			19
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/rpBOz0X1i5",
+					"expanded_url": "https://twitter.com/mattenb/status/1120581917391376384",
+					"display_url": "twitter.com/mattenb/status…",
+					"indices": [
+						20,
+						43
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 188,
+			"friends_count": 139,
+			"listed_count": 30,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 669,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15766,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1120581917391376400,
+		"quoted_status_id_str": "1120581917391376384",
+		"quoted_status_permalink": {
+			"url": "https://t.co/rpBOz0X1i5",
+			"expanded": "https://twitter.com/mattenb/status/1120581917391376384",
+			"display": "twitter.com/mattenb/status…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Apr 23 06:55:23 +0000 2019",
+			"id": 1120581917391376400,
+			"id_str": "1120581917391376384",
+			"full_text": "@timpritlove Du magst wahre Meere aus Plastikfolien?",
+			"truncated": false,
+			"display_text_range": [
+				13,
+				52
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "timpritlove",
+						"name": "Tim Pritlove",
+						"id": 11268812,
+						"id_str": "11268812",
+						"indices": [
+							0,
+							12
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+			"in_reply_to_status_id": 1120580210867560400,
+			"in_reply_to_status_id_str": "1120580210867560448",
+			"in_reply_to_user_id": 11268812,
+			"in_reply_to_user_id_str": "11268812",
+			"in_reply_to_screen_name": "timpritlove",
+			"user": {
+				"id": 150971723,
+				"id_str": "150971723",
+				"name": "Matzbach",
+				"screen_name": "mattenb",
+				"location": "Britz, Bärlin",
+				"description": "Universaldilettant! Trotzdem: Allet wird jut!",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 959,
+				"friends_count": 2244,
+				"listed_count": 22,
+				"created_at": "Wed Jun 02 08:28:00 +0000 2010",
+				"favourites_count": 47114,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 26487,
+				"lang": "de",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/670321402352943104/XiNXVssh_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/670321402352943104/XiNXVssh_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/150971723/1453822235",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "de"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Facepalm der Woche: https://t.co/rpBOz0X1i5"
+	},
+	{
 		"created_at": "Wed Apr 17 18:56:23 +0000 2019",
 		"id": 1118589038166990800,
 		"id_str": "1118589038166990848",
