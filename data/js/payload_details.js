512b043d463ec8ace5b53ad5adfb5c1d7f673526
Stefan Seiz <seiz@imd.net>
2019-05-07 09:14:22 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 14732,
-	"created_at": "Mon May 06 07:14:19 +0000 2019"
+	"tweets": 14733,
+	"created_at": "Tue May 07 07:14:21 +0000 2019"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,243 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Mon May 06 15:23:52 +0000 2019",
+		"id": 1125420926349320200,
+		"id_str": "1125420926349320192",
+		"full_text": "Hat mir gefallen. Hat er gut gemacht, der Frank-Walter. Auch das mit Dennis. https://t.co/HkcRm0Rp1g",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			76
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HkcRm0Rp1g",
+					"expanded_url": "https://twitter.com/spreeblick/status/1125391503562559488",
+					"display_url": "twitter.com/spreeblick/sta…",
+					"indices": [
+						77,
+						100
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 188,
+			"friends_count": 137,
+			"listed_count": 30,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 671,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15773,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1125391503562559500,
+		"quoted_status_id_str": "1125391503562559488",
+		"quoted_status_permalink": {
+			"url": "https://t.co/HkcRm0Rp1g",
+			"expanded": "https://twitter.com/spreeblick/status/1125391503562559488",
+			"display": "twitter.com/spreeblick/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Mon May 06 13:26:57 +0000 2019",
+			"id": 1125391503562559500,
+			"id_str": "1125391503562559488",
+			"full_text": "Der Mitschnitt der Rede von Bundespräsident Frank-Walter Steinmeier bei der @republica #rp19: \nhttps://t.co/y3WwCQbNTl",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				118
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "rp19",
+						"indices": [
+							87,
+							92
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "republica",
+						"name": "re:publica",
+						"id": 9824472,
+						"id_str": "9824472",
+						"indices": [
+							76,
+							86
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/y3WwCQbNTl",
+						"expanded_url": "https://youtu.be/6hfFGM7UTK4",
+						"display_url": "youtu.be/6hfFGM7UTK4",
+						"indices": [
+							95,
+							118
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3782931,
+				"id_str": "3782931",
+				"name": "Johnny Haeusler",
+				"screen_name": "spreeblick",
+				"location": "Berlin",
+				"description": "Gründer: Spreeblick @republica @tincon und Plan B. Außerdem Kolumnist und Autor. Favs bedeuten auch mal: Danke. Raum für persönliche Notizen.",
+				"url": "https://t.co/dIfZzCpeTo",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/dIfZzCpeTo",
+								"expanded_url": "http://spreeblick.com",
+								"display_url": "spreeblick.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 114479,
+				"friends_count": 7326,
+				"listed_count": 1828,
+				"created_at": "Sun Apr 08 12:02:35 +0000 2007",
+				"favourites_count": 17710,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 32339,
+				"lang": "de",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/876751941208088576/N7cb3Liu_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/876751941208088576/N7cb3Liu_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3782931/1549315302",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 18,
+			"favorite_count": 42,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "de"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Hat mir gefallen. Hat er gut gemacht, der Frank-Walter. Auch das mit Dennis. https://t.co/HkcRm0Rp1g"
+	},
+	{
 		"created_at": "Sun May 05 17:37:10 +0000 2019",
 		"id": 1125092084933910500,
 		"id_str": "1125092084933910533",
