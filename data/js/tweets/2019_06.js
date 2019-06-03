81c5d1cce93bed872e0ca50e876698517e890562
Stefan Seiz <seiz@imd.net>
2019-06-03 09:14:20 +0200

    Added 1 tweets

--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,279 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Mon Jun 03 05:46:27 +0000 2019",
+		"id": 1135422475054080000,
+		"id_str": "1135422475054080001",
+		"full_text": "Herrlich. https://t.co/ssRhjKKYS6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			9
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ssRhjKKYS6",
+					"expanded_url": "https://twitter.com/insideX/status/1134194613017858048",
+					"display_url": "twitter.com/insideX/status…",
+					"indices": [
+						10,
+						33
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 187,
+			"friends_count": 138,
+			"listed_count": 30,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 657,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15791,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1134194613017858000,
+		"quoted_status_id_str": "1134194613017858048",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ssRhjKKYS6",
+			"expanded": "https://twitter.com/insideX/status/1134194613017858048",
+			"display": "twitter.com/insideX/status…"
+		},
+		"quoted_status": {
+			"created_at": "Thu May 30 20:27:22 +0000 2019",
+			"id": 1134194613017858000,
+			"id_str": "1134194613017858048",
+			"full_text": "Quelle Internet https://t.co/CkS6L3HqD2",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				15
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1134194396126163000,
+						"id_str": "1134194396126162944",
+						"indices": [
+							16,
+							39
+						],
+						"media_url": "http://pbs.twimg.com/media/D713y0dWsAA9b-0.png",
+						"media_url_https": "https://pbs.twimg.com/media/D713y0dWsAA9b-0.png",
+						"url": "https://t.co/CkS6L3HqD2",
+						"display_url": "pic.twitter.com/CkS6L3HqD2",
+						"expanded_url": "https://twitter.com/insideX/status/1134194613017858048/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 520,
+								"h": 260,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 520,
+								"h": 260,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 520,
+								"h": 260,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1134194396126163000,
+						"id_str": "1134194396126162944",
+						"indices": [
+							16,
+							39
+						],
+						"media_url": "http://pbs.twimg.com/media/D713y0dWsAA9b-0.png",
+						"media_url_https": "https://pbs.twimg.com/media/D713y0dWsAA9b-0.png",
+						"url": "https://t.co/CkS6L3HqD2",
+						"display_url": "pic.twitter.com/CkS6L3HqD2",
+						"expanded_url": "https://twitter.com/insideX/status/1134194613017858048/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 520,
+								"h": 260,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 520,
+								"h": 260,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 520,
+								"h": 260,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 14857351,
+				"id_str": "14857351",
+				"name": "Kollektiv Raufaser",
+				"screen_name": "insideX",
+				"location": "Ex-Gallus jetzt Lindlar im Bergischen Land",
+				"description": "Serendipityianer, Papa, Datenschützer,  idiotophob und im uncanny Valley. \n@insidex@mastodon.social",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5919,
+				"friends_count": 2252,
+				"listed_count": 409,
+				"created_at": "Wed May 21 14:54:30 +0000 2008",
+				"favourites_count": 26110,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 50257,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1323753452/twitter_avatar_5_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1323753452/twitter_avatar_5_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/14857351/1524897921",
+				"profile_link_color": "FF3300",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "D5D5D8",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 86,
+			"favorite_count": 223,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "fr"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Herrlich. https://t.co/ssRhjKKYS6"
+	},
+	{
 		"created_at": "Sat Jun 01 12:26:20 +0000 2019",
 		"id": 1134798332617011200,
 		"id_str": "1134798332617011202",
