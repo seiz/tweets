bee3480ad257f8cdcc8bc7b9ce0e14d9cf761d83
Stefan Seiz <seiz@imd.net>
2019-06-26 09:14:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 14764,
-	"created_at": "Mon Jun 24 07:14:20 +0000 2019"
+	"tweets": 14765,
+	"created_at": "Wed Jun 26 07:14:20 +0000 2019"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Tue Jun 25 14:35:41 +0000 2019",
+		"id": 1143528193384702000,
+		"id_str": "1143528193384701954",
+		"full_text": "@KilianMuster @affinitybyserif I have tons of fireworks-files which affinity can't natively open (keeping vectors editable).",
+		"truncated": false,
+		"display_text_range": [
+			31,
+			124
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "KilianMuster",
+					"name": "☰ 𝗞𝗶𝗟𝗶𝝙𝗡 ☰",
+					"id": 906773598831964200,
+					"id_str": "906773598831964160",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "affinitybyserif",
+					"name": "Affinity",
+					"id": 2558446890,
+					"id_str": "2558446890",
+					"indices": [
+						14,
+						30
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1143510808246120400,
+		"in_reply_to_status_id_str": "1143510808246120448",
+		"in_reply_to_user_id": 906773598831964200,
+		"in_reply_to_user_id_str": "906773598831964160",
+		"in_reply_to_screen_name": "KilianMuster",
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 186,
+			"friends_count": 138,
+			"listed_count": 30,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 664,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15807,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@KilianMuster @affinitybyserif I have tons of fireworks-files which affinity can't natively open (keeping vectors editable)."
+	},
+	{
 		"created_at": "Sun Jun 23 18:02:07 +0000 2019",
 		"id": 1142855366583160800,
 		"id_str": "1142855366583160834",
