c9420d5ae33a5a0ad1a4ccc8e5162395675613ef
Stefan Seiz <seiz@imd.net>
2020-04-02 09:14:21 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 14962,
-	"created_at": "Wed Apr 01 07:14:20 +0000 2020"
+	"tweets": 14964,
+	"created_at": "Thu Apr 02 07:14:20 +0000 2020"
 }
---
+++ data/js/tweets/2020_04.js
@@ -0,0 +1,380 @@
+Grailbird.data.tweets_2020_04 = 
+[
+	{
+		"created_at": "Wed Apr 01 18:37:49 +0000 2020",
+		"id": 1245420116956897300,
+		"id_str": "1245420116956897281",
+		"full_text": "@reichenstein https://t.co/gOYoeCTnla",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			37
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "reichenstein",
+					"name": "Oliver Reichenstein",
+					"id": 17435847,
+					"id_str": "17435847",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/gOYoeCTnla",
+					"expanded_url": "https://twitter.com/palantir/status/1245417070759731205",
+					"display_url": "twitter.com/palantir/statu…",
+					"indices": [
+						14,
+						37
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1245418443010183200,
+		"in_reply_to_status_id_str": "1245418443010183168",
+		"in_reply_to_user_id": 17435847,
+		"in_reply_to_user_id_str": "17435847",
+		"in_reply_to_screen_name": "reichenstein",
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 184,
+			"friends_count": 137,
+			"listed_count": 28,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 693,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16073,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1245417070759731200,
+		"quoted_status_id_str": "1245417070759731205",
+		"quoted_status_permalink": {
+			"url": "https://t.co/gOYoeCTnla",
+			"expanded": "https://twitter.com/palantir/status/1245417070759731205",
+			"display": "twitter.com/palantir/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Apr 01 18:25:43 +0000 2020",
+			"id": 1245417070759731200,
+			"id_str": "1245417070759731205",
+			"full_text": "@HNSGR @BAG_OFSP_UFSP You're confusing us with @palantirtech; we're unaffiliated. https://t.co/4GU1FZyGBM",
+			"truncated": false,
+			"display_text_range": [
+				22,
+				105
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "HNSGR",
+						"name": "Hannes Grassegger",
+						"id": 1607859030,
+						"id_str": "1607859030",
+						"indices": [
+							0,
+							6
+						]
+					},
+					{
+						"screen_name": "BAG_OFSP_UFSP",
+						"name": "BAG – OFSP – UFSP",
+						"id": 326650532,
+						"id_str": "326650532",
+						"indices": [
+							7,
+							21
+						]
+					},
+					{
+						"screen_name": "PalantirTech",
+						"name": "Palantir",
+						"id": 37959591,
+						"id_str": "37959591",
+						"indices": [
+							47,
+							60
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/4GU1FZyGBM",
+						"expanded_url": "https://www.palantir.net/blog/palantir-you-re-looking",
+						"display_url": "palantir.net/blog/palantir-…",
+						"indices": [
+							82,
+							105
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://www.hubspot.com/\" rel=\"nofollow\">HubSpot</a>",
+			"in_reply_to_status_id": 1245416387293708300,
+			"in_reply_to_status_id_str": "1245416387293708296",
+			"in_reply_to_user_id": 1607859030,
+			"in_reply_to_user_id_str": "1607859030",
+			"in_reply_to_screen_name": "HNSGR",
+			"user": {
+				"id": 24758605,
+				"id_str": "24758605",
+				"name": "Palantir.net",
+				"screen_name": "palantir",
+				"location": "Chicago and beyond",
+				"description": "Palantir® is a full-service web firm that creates engaging online experiences using @Drupal and other open source technologies. We make good, inside and out.",
+				"url": "http://t.co/cTL7hg0wQN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/cTL7hg0wQN",
+								"expanded_url": "http://palantir.net",
+								"display_url": "palantir.net",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3649,
+				"friends_count": 419,
+				"listed_count": 327,
+				"created_at": "Mon Mar 16 20:07:08 +0000 2009",
+				"favourites_count": 729,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 5352,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "11A9A1",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/3783889548/21de6bc9678823824946ffa3f9746dce_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/3783889548/21de6bc9678823824946ffa3f9746dce_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/24758605/1560381708",
+				"profile_link_color": "669999",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "E0EFEB",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 4,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "@reichenstein https://t.co/gOYoeCTnla"
+	},
+	{
+		"created_at": "Wed Apr 01 14:30:56 +0000 2020",
+		"id": 1245357988522705000,
+		"id_str": "1245357988522704896",
+		"full_text": "@drikkes Hier: https://t.co/NO0uTp53lo",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			38
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "drikkes",
+					"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+					"id": 15378060,
+					"id_str": "15378060",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/NO0uTp53lo",
+					"expanded_url": "https://ille.shop",
+					"display_url": "ille.shop",
+					"indices": [
+						15,
+						38
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1245345178648535000,
+		"in_reply_to_status_id_str": "1245345178648535046",
+		"in_reply_to_user_id": 15378060,
+		"in_reply_to_user_id_str": "15378060",
+		"in_reply_to_screen_name": "drikkes",
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 184,
+			"friends_count": 137,
+			"listed_count": 28,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 693,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16073,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "@drikkes Hier: https://t.co/NO0uTp53lo"
+	}
+]
