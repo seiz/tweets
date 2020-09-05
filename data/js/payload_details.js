6704c6fa739b7dc5dcd283b525bd9bfe79c6390b
Stefan Seiz <seiz@imd.net>
2020-09-05 09:14:21 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15080,
-	"created_at": "Wed Sep 02 07:14:19 +0000 2020"
+	"tweets": 15081,
+	"created_at": "Sat Sep 05 07:14:19 +0000 2020"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,320 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Fri Sep 04 12:00:57 +0000 2020",
+		"id": 1301852749299167200,
+		"id_str": "1301852749299167232",
+		"full_text": "Wow! Viel Elektronikschrott für nix. https://t.co/iRDFPYXOYd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			36
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/iRDFPYXOYd",
+					"expanded_url": "https://twitter.com/Foone/status/1301707401024827392",
+					"display_url": "twitter.com/Foone/status/1…",
+					"indices": [
+						37,
+						60
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/5UFfaMMOCQ",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/5UFfaMMOCQ",
+							"expanded_url": "https://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 182,
+			"friends_count": 135,
+			"listed_count": 27,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 708,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16214,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1301707401024827400,
+		"quoted_status_id_str": "1301707401024827392",
+		"quoted_status_permalink": {
+			"url": "https://t.co/iRDFPYXOYd",
+			"expanded": "https://twitter.com/Foone/status/1301707401024827392",
+			"display": "twitter.com/Foone/status/1…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Sep 04 02:23:23 +0000 2020",
+			"id": 1301707401024827400,
+			"id_str": "1301707401024827392",
+			"full_text": "I saw a tweet recently that I wanted to confirm. Sadly I can't find it right now, but it was about digital pregnancy tests.\nSo, I went out and grabbed a 2-pack for 7 dollars: let's tear it down! https://t.co/1a0drwi7N5",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				194
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1301707172607225900,
+						"id_str": "1301707172607225856",
+						"indices": [
+							195,
+							218
+						],
+						"media_url": "http://pbs.twimg.com/media/EhCXxtjU8AATFux.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EhCXxtjU8AATFux.jpg",
+						"url": "https://t.co/1a0drwi7N5",
+						"display_url": "pic.twitter.com/1a0drwi7N5",
+						"expanded_url": "https://twitter.com/Foone/status/1301707401024827392/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 667,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1139,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 378,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1301707172607225900,
+						"id_str": "1301707172607225856",
+						"indices": [
+							195,
+							218
+						],
+						"media_url": "http://pbs.twimg.com/media/EhCXxtjU8AATFux.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EhCXxtjU8AATFux.jpg",
+						"url": "https://t.co/1a0drwi7N5",
+						"display_url": "pic.twitter.com/1a0drwi7N5",
+						"expanded_url": "https://twitter.com/Foone/status/1301707401024827392/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 667,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1139,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 378,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 13502732,
+				"id_str": "13502732",
+				"name": "foone",
+				"screen_name": "Foone",
+				"location": "Milpitas, CA",
+				"description": "Hardware / software necromancer, collector of Weird Stuff, maker of Death Generators. (they/them)\nPatreon: https://t.co/6UkafJHkbD\nko-fi: https://t.co/6AzbhTO0lK",
+				"url": "https://t.co/2PlYsHBSvH",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/2PlYsHBSvH",
+								"expanded_url": "http://floppy.foone.org",
+								"display_url": "floppy.foone.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/6UkafJHkbD",
+								"expanded_url": "http://patreon.com/foone",
+								"display_url": "patreon.com/foone",
+								"indices": [
+									107,
+									130
+								]
+							},
+							{
+								"url": "https://t.co/6AzbhTO0lK",
+								"expanded_url": "http://ko-fi.com/fooneturing",
+								"display_url": "ko-fi.com/fooneturing",
+								"indices": [
+									138,
+									161
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 79715,
+				"friends_count": 545,
+				"listed_count": 647,
+				"created_at": "Fri Feb 15 03:51:09 +0000 2008",
+				"favourites_count": 67490,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 150148,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/880340410333143040/-s_buYC0_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/880340410333143040/-s_buYC0_normal.jpg",
+				"profile_link_color": "008080",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7440,
+			"favorite_count": 23569,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Wow! Viel Elektronikschrott für nix. https://t.co/iRDFPYXOYd"
+	},
+	{
 		"created_at": "Tue Sep 01 12:22:12 +0000 2020",
 		"id": 1300770932768866300,
 		"id_str": "1300770932768866306",
