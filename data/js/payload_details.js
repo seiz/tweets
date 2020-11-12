73886622a8b0dabe5c1de783a56560f2e2ba0f42
Stefan Seiz <seiz@imd.net>
2020-11-12 08:14:21 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15153,
-	"created_at": "Wed Nov 11 07:14:20 +0000 2020"
+	"tweets": 15154,
+	"created_at": "Thu Nov 12 07:14:20 +0000 2020"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,244 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 11 17:34:08 +0000 2020",
+		"id": 1326578971476103200,
+		"id_str": "1326578971476103168",
+		"full_text": "Oh FFS! How can a system be that broken? https://t.co/hYNx6YL0SZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			40
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/hYNx6YL0SZ",
+					"expanded_url": "https://twitter.com/hukl/status/1326449119368519680",
+					"display_url": "twitter.com/hukl/status/13…",
+					"indices": [
+						41,
+						64
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/5UFfaMMOCQ",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/5UFfaMMOCQ",
+							"expanded_url": "https://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 183,
+			"friends_count": 134,
+			"listed_count": 26,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 718,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16304,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1326449119368519700,
+		"quoted_status_id_str": "1326449119368519680",
+		"quoted_status_permalink": {
+			"url": "https://t.co/hYNx6YL0SZ",
+			"expanded": "https://twitter.com/hukl/status/1326449119368519680",
+			"display": "twitter.com/hukl/status/13…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Nov 11 08:58:08 +0000 2020",
+			"id": 1326449119368519700,
+			"id_str": "1326449119368519680",
+			"full_text": "What if a US presidential candidate refuses to concede after an election?\n\nhttps://t.co/uviVMMvecv",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				98
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/uviVMMvecv",
+						"expanded_url": "https://www.youtube.com/watch?v=WZWRhLW7Y8w",
+						"display_url": "youtube.com/watch?v=WZWRhL…",
+						"indices": [
+							75,
+							98
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 14842853,
+				"id_str": "14842853",
+				"name": "hukl",
+				"screen_name": "hukl",
+				"location": "",
+				"description": "programmer, backend developer, guitar player, podcaster, lecturer, home studio enthusiast, autodidact. Flatten the CO2 curve: https://t.co/bsAkiU3NHn",
+				"url": "https://t.co/XmFqc5ccLz",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/XmFqc5ccLz",
+								"expanded_url": "http://smyck.net/",
+								"display_url": "smyck.net",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/bsAkiU3NHn",
+								"expanded_url": "http://climeworks.refr.cc/huklco2",
+								"display_url": "climeworks.refr.cc/huklco2",
+								"indices": [
+									126,
+									149
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 11343,
+				"friends_count": 252,
+				"listed_count": 309,
+				"created_at": "Tue May 20 06:35:28 +0000 2008",
+				"favourites_count": 845,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 21191,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1059404207638409218/ljqPmlgI_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1059404207638409218/ljqPmlgI_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/14842853/1484821449",
+				"profile_link_color": "009999",
+				"profile_sidebar_border_color": "EEEEEE",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 9,
+			"favorite_count": 20,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Oh FFS! How can a system be that broken? https://t.co/hYNx6YL0SZ"
+	},
+	{
 		"created_at": "Tue Nov 10 19:52:32 +0000 2020",
 		"id": 1326251413882671000,
 		"id_str": "1326251413882671104",
