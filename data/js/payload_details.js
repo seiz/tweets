858b0f816db5af8206e9aeba3d36cb36720e03c3
Stefan Seiz <seiz@imd.net>
2020-11-19 08:14:23 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15183,
-	"created_at": "Wed Nov 18 07:14:20 +0000 2020"
+	"tweets": 15184,
+	"created_at": "Thu Nov 19 07:14:21 +0000 2020"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 18 20:09:51 +0000 2020",
+		"id": 1329154873921826800,
+		"id_str": "1329154873921826822",
+		"full_text": "@die_frausim @wunderbayer Das hat halt Nebenwirkungen. Z.B.:\n- controllcenter muss von ganz oben rechts hergewischt werden. Das verursacht Schmerzen in meinem rechten Daumen\n- dauernde, unbeabsichtigt erstellte Screenshots, weil sich die Tasten gegenüber liegen\n…",
+		"truncated": false,
+		"display_text_range": [
+			26,
+			263
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "die_frausim",
+					"name": "YvonneSim",
+					"id": 1236550988200378400,
+					"id_str": "1236550988200378368",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "wunderbayer",
+					"name": "Erik",
+					"id": 14510685,
+					"id_str": "14510685",
+					"indices": [
+						13,
+						25
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1329133225470533600,
+		"in_reply_to_status_id_str": "1329133225470533637",
+		"in_reply_to_user_id": 1236550988200378400,
+		"in_reply_to_user_id_str": "1236550988200378368",
+		"in_reply_to_screen_name": "die_frausim",
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/5UFfaMMOCQ",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/5UFfaMMOCQ",
+							"expanded_url": "https://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 182,
+			"friends_count": 134,
+			"listed_count": 26,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 720,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16340,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@die_frausim @wunderbayer Das hat halt Nebenwirkungen. Z.B.:\n- controllcenter muss von ganz oben rechts hergewischt werden. Das verursacht Schmerzen in meinem rechten Daumen\n- dauernde, unbeabsichtigt erstellte Screenshots, weil sich die Tasten gegenüber liegen\n…"
+	},
+	{
 		"created_at": "Tue Nov 17 21:25:00 +0000 2020",
 		"id": 1328811398835658800,
 		"id_str": "1328811398835658753",
