c3daa8e40b59418457a0e8587a9afb6ede41e272
Stefan Seiz <seiz@imd.net>
2021-01-04 08:14:21 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15240,
-	"created_at": "Sun Jan 03 07:14:19 +0000 2021"
+	"tweets": 15241,
+	"created_at": "Mon Jan 04 07:14:20 +0000 2021"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sun Jan 03 15:24:54 +0000 2021",
+		"id": 1345753005979742200,
+		"id_str": "1345753005979742210",
+		"full_text": "Ich sag’s mal so: ich will diese Krankheit trotzdem nicht haben. https://t.co/hsjDZKJSIb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			64
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/hsjDZKJSIb",
+					"expanded_url": "https://twitter.com/ChristophKappes/status/1345743929250111489",
+					"display_url": "twitter.com/ChristophKappe…",
+					"indices": [
+						65,
+						88
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/5UFfaMMOCQ",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/5UFfaMMOCQ",
+							"expanded_url": "https://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 183,
+			"friends_count": 135,
+			"listed_count": 26,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 726,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16415,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1345743929250111500,
+		"quoted_status_id_str": "1345743929250111489",
+		"quoted_status_permalink": {
+			"url": "https://t.co/hsjDZKJSIb",
+			"expanded": "https://twitter.com/ChristophKappes/status/1345743929250111489",
+			"display": "twitter.com/ChristophKappe…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Jan 03 14:48:50 +0000 2021",
+			"id": 1345743929250111500,
+			"id_str": "1345743929250111489",
+			"full_text": "Dieser Text eines LMU-Soziologen behauptet, es gäbe im Jahrsvergleich 2020 keine Sterberaten oberhalb des Erwartungswertes &amp; die Übersterblichkeit berücksichtige nicht die soziodemographische Verschiebung der Alterskohorten, wenn ich das richtig verstehe. https://t.co/HxBfyvRuil",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				259
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/HxBfyvRuil",
+						"expanded_url": "https://twitter.com/telepolis_news/status/1345690395280334852",
+						"display_url": "twitter.com/telepolis_news…",
+						"indices": [
+							260,
+							283
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 19961453,
+				"id_str": "19961453",
+				"name": "Christoph Kappes",
+				"screen_name": "ChristophKappes",
+				"location": "Hamburg/München, Germany",
+				"description": "Follow you & follow me, ahaaa. Ich habe meine Meinung nur, solange ich keine bessere habe. Made with ❤ in Paris.",
+				"url": "https://t.co/gwZFd0Jjrq",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/gwZFd0Jjrq",
+								"expanded_url": "http://www.christophkappes.de",
+								"display_url": "christophkappes.de",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 8812,
+				"friends_count": 3132,
+				"listed_count": 417,
+				"created_at": "Tue Feb 03 09:14:01 +0000 2009",
+				"favourites_count": 52906,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 68504,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFCC4D",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1269736040799121414/2L_kRqxq_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1269736040799121414/2L_kRqxq_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/19961453/1585818583",
+				"profile_link_color": "FA743E",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "C9CCC8",
+				"profile_text_color": "6B696B",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1345690395280334800,
+			"quoted_status_id_str": "1345690395280334852",
+			"quoted_status_permalink": {
+				"url": "https://t.co/HxBfyvRuil",
+				"expanded": "https://twitter.com/telepolis_news/status/1345690395280334852",
+				"display": "twitter.com/telepolis_news…"
+			},
+			"retweet_count": 4,
+			"favorite_count": 9,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "de"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Ich sag’s mal so: ich will diese Krankheit trotzdem nicht haben. https://t.co/hsjDZKJSIb"
+	},
+	{
 		"created_at": "Sat Jan 02 17:58:20 +0000 2021",
 		"id": 1345429233938669600,
 		"id_str": "1345429233938669569",
