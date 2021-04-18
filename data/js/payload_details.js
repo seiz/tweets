4af4b2b70288693338f32cd5eff8b3173fb94bec
Stefan Seiz <seiz@imd.net>
2021-04-18 09:14:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15393,
-	"created_at": "Sat Apr 17 07:14:25 +0000 2021"
+	"tweets": 15394,
+	"created_at": "Sun Apr 18 07:14:19 +0000 2021"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Sun Apr 18 06:03:55 +0000 2021",
+		"id": 1383662556468764700,
+		"id_str": "1383662556468764673",
+		"full_text": "@zeitonline @zeitwissen @mariebrennt Könntet Ihr bitte noch den RSS-Feed mit der neuen Folge aktualisieren? Danke!",
+		"truncated": false,
+		"display_text_range": [
+			37,
+			114
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "zeitonline",
+					"name": "ZEIT ONLINE",
+					"id": 5715752,
+					"id_str": "5715752",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "zeitwissen",
+					"name": "ZEIT Wissen",
+					"id": 1168803702,
+					"id_str": "1168803702",
+					"indices": [
+						12,
+						23
+					]
+				},
+				{
+					"screen_name": "mariebrennt",
+					"name": "Marie Brand",
+					"id": 2322286388,
+					"id_str": "2322286388",
+					"indices": [
+						24,
+						36
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1383655085687804000,
+		"in_reply_to_status_id_str": "1383655085687803904",
+		"in_reply_to_user_id": 5715752,
+		"in_reply_to_user_id_str": "5715752",
+		"in_reply_to_screen_name": "zeitonline",
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/5UFfaMMOCQ",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/5UFfaMMOCQ",
+							"expanded_url": "https://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 183,
+			"friends_count": 134,
+			"listed_count": 26,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 731,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16606,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@zeitonline @zeitwissen @mariebrennt Könntet Ihr bitte noch den RSS-Feed mit der neuen Folge aktualisieren? Danke!"
+	},
+	{
 		"created_at": "Fri Apr 16 18:12:20 +0000 2021",
 		"id": 1383016084090056700,
 		"id_str": "1383121095017046016",
