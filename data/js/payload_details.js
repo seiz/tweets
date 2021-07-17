9a374c58e1f11d63f4e23d6d2e81be19acac8c8d
Stefan Seiz <seiz@imd.net>
2021-07-17 09:14:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15517,
-	"created_at": "Fri Jul 16 07:14:19 +0000 2021"
+	"tweets": 15518,
+	"created_at": "Sat Jul 17 07:14:19 +0000 2021"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Fri Jul 16 12:17:43 +0000 2021",
+		"id": 1416009146252599300,
+		"id_str": "1416009146252599296",
+		"full_text": "@techniktagebuch Die separaten Ein/Aus Knöpfe bieten vermutlich den Vorteil, dass man den zustand des Fernsehers nicht kennen muss, um die Funktion des Knopfes einzuschätzen. Man kann quasi blind auf \"Aus\" drücken und kann sich sicher sein, nicht ggf. eingeschaltet zu haben weil schon aus war…",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			294
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "techniktagebuch",
+					"name": "Techniktagebuch",
+					"id": 2359474651,
+					"id_str": "2359474651",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1416004906721370000,
+		"in_reply_to_status_id_str": "1416004906721370112",
+		"in_reply_to_user_id": 2359474651,
+		"in_reply_to_user_id_str": "2359474651",
+		"in_reply_to_screen_name": "techniktagebuch",
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/5UFfaMMOCQ",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/5UFfaMMOCQ",
+							"expanded_url": "https://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 183,
+			"friends_count": 136,
+			"listed_count": 26,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 747,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16765,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@techniktagebuch Die separaten Ein/Aus Knöpfe bieten vermutlich den Vorteil, dass man den zustand des Fernsehers nicht kennen muss, um die Funktion des Knopfes einzuschätzen. Man kann quasi blind auf \"Aus\" drücken und kann sich sicher sein, nicht ggf. eingeschaltet zu haben weil schon aus war…"
+	},
+	{
 		"created_at": "Thu Jul 15 22:39:04 +0000 2021",
 		"id": 1415803128478306300,
 		"id_str": "1415803128478306310",
