133686a1676ea1e7f392bb2ee995a79147cafec2
Stefan Seiz <seiz@imd.net>
2021-10-07 09:14:20 +0200

    Added 1 tweets

--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Wed Oct 06 14:54:07 +0000 2021",
+		"id": 1445764312396488700,
+		"id_str": "1445764312396488710",
+		"full_text": "Im Gegensatz zu @artcom vs. Google (Google Earth), konnten wir damals Apple (iPhone) nicht verklagen. Wir hatten kein Patent auf das User-interface und auch nicht das nötige Kleingeld.\n\n#almostbillionaire https://t.co/nUmx9oXb6J",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			204
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "almostbillionaire",
+					"indices": [
+						186,
+						204
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "artcom",
+					"name": "ART+COM Studios",
+					"id": 15296460,
+					"id_str": "15296460",
+					"indices": [
+						16,
+						23
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1445764303865335800,
+					"id_str": "1445764303865335828",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/media/FBBi-XDVUBQPhWg.png",
+					"media_url_https": "https://pbs.twimg.com/media/FBBi-XDVUBQPhWg.png",
+					"url": "https://t.co/nUmx9oXb6J",
+					"display_url": "pic.twitter.com/nUmx9oXb6J",
+					"expanded_url": "https://twitter.com/seiz/status/1445764312396488710/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 322,
+							"h": 242,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 322,
+							"h": 242,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 322,
+							"h": 242,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1445764303865335800,
+					"id_str": "1445764303865335828",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/media/FBBi-XDVUBQPhWg.png",
+					"media_url_https": "https://pbs.twimg.com/media/FBBi-XDVUBQPhWg.png",
+					"url": "https://t.co/nUmx9oXb6J",
+					"display_url": "pic.twitter.com/nUmx9oXb6J",
+					"expanded_url": "https://twitter.com/seiz/status/1445764312396488710/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 322,
+							"h": 242,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 322,
+							"h": 242,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 322,
+							"h": 242,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1445764304758710300,
+					"id_str": "1445764304758710291",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/media/FBBi-aYVIBMSvsv.png",
+					"media_url_https": "https://pbs.twimg.com/media/FBBi-aYVIBMSvsv.png",
+					"url": "https://t.co/nUmx9oXb6J",
+					"display_url": "pic.twitter.com/nUmx9oXb6J",
+					"expanded_url": "https://twitter.com/seiz/status/1445764312396488710/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 322,
+							"h": 242,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 322,
+							"h": 242,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 322,
+							"h": 242,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/5UFfaMMOCQ",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/5UFfaMMOCQ",
+							"expanded_url": "https://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 183,
+			"friends_count": 134,
+			"listed_count": 26,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 741,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16849,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Im Gegensatz zu @artcom vs. Google (Google Earth), konnten wir damals Apple (iPhone) nicht verklagen. Wir hatten kein Patent auf das User-interface und auch nicht das nötige Kleingeld.\n\n#almostbillionaire https://t.co/nUmx9oXb6J"
+	},
+	{
 		"created_at": "Mon Oct 04 16:33:46 +0000 2021",
 		"id": 1445064611678523400,
 		"id_str": "1445064611678523405",
