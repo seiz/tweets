a7ef6b1bcec3097325a1440dada9be7cf949b255
Stefan Seiz <seiz@imd.net>
2022-02-07 08:14:20 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15663,
-	"created_at": "Sun Feb 06 07:14:19 +0000 2022"
+	"tweets": 15664,
+	"created_at": "Mon Feb 07 07:14:19 +0000 2022"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,243 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Sun Feb 06 15:25:45 +0000 2022",
+		"id": 1490345981132562400,
+		"id_str": "1490345981132562447",
+		"full_text": "Still do. Still does. https://t.co/q7zNH082Lj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			21
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/q7zNH082Lj",
+					"expanded_url": "https://twitter.com/siegel/status/1490327021356036096",
+					"display_url": "twitter.com/siegel/status/…",
+					"indices": [
+						22,
+						45
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/5UFfaMMOCQ",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/5UFfaMMOCQ",
+							"expanded_url": "https://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 181,
+			"friends_count": 134,
+			"listed_count": 0,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 776,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16963,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1490327021356036000,
+		"quoted_status_id_str": "1490327021356036096",
+		"quoted_status_permalink": {
+			"url": "https://t.co/q7zNH082Lj",
+			"expanded": "https://twitter.com/siegel/status/1490327021356036096",
+			"display": "twitter.com/siegel/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Feb 06 14:10:25 +0000 2022",
+			"id": 1490327021356036000,
+			"id_str": "1490327021356036096",
+			"full_text": "Can confirm. https://t.co/WKsFPoRWlk",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				12
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/WKsFPoRWlk",
+						"expanded_url": "https://twitter.com/aloria/status/1490026515970543616",
+						"display_url": "twitter.com/aloria/status/…",
+						"indices": [
+							13,
+							36
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 8021722,
+				"id_str": "8021722",
+				"name": "Rich Siegel",
+				"screen_name": "siegel",
+				"location": "Secret Volcano Lair in Conhampshimass Island",
+				"description": "I am a finite state machine that uses Macs to turn caffeine into bugs. Occasional KitchenAid mixer content. #Mixerology He/him.",
+				"url": "https://t.co/gZ8tQ6UZAB",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/gZ8tQ6UZAB",
+								"expanded_url": "https://www.barebones.com/",
+								"display_url": "barebones.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 6957,
+				"friends_count": 1278,
+				"listed_count": 347,
+				"created_at": "Tue Aug 07 12:47:09 +0000 2007",
+				"favourites_count": 30631,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 58137,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/477279893365862400/U5-opzr1_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/477279893365862400/U5-opzr1_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/8021722/1468953160",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1490026515970543600,
+			"quoted_status_id_str": "1490026515970543616",
+			"quoted_status_permalink": {
+				"url": "https://t.co/WKsFPoRWlk",
+				"expanded": "https://twitter.com/aloria/status/1490026515970543616",
+				"display": "twitter.com/aloria/status/…"
+			},
+			"retweet_count": 4,
+			"favorite_count": 62,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Still do. Still does. https://t.co/q7zNH082Lj"
+	},
+	{
 		"created_at": "Sat Feb 05 18:46:27 +0000 2022",
 		"id": 1490034098483703800,
 		"id_str": "1490034098483703822",
