56f80075bd41b1c2590a1b084e6c5ed15285ce80
Stefan Seiz <seiz@imd.net>
2022-02-20 08:14:20 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15669,
-	"created_at": "Fri Feb 18 07:14:19 +0000 2022"
+	"tweets": 15670,
+	"created_at": "Sun Feb 20 07:14:19 +0000 2022"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,134 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Sat Feb 19 19:10:52 +0000 2022",
+		"id": 1495113674327048200,
+		"id_str": "1495113674327048199",
+		"full_text": "@techniktagebuch Ich darf noch nen Link nachliefern: https://t.co/QGaWrHqvs9ÅMÅŽÕÑ&amp;crid=3G32FDCGODGB9&amp;keywords=ansmann+steckdose+timer+1260&amp;qid=1645297782&amp;sprefix=ansmann+steckdosen+timer+1260%2Caps%2C98&amp;sr=8-4",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			230
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "techniktagebuch",
+					"name": "Techniktagebuch",
+					"id": 2359474651,
+					"id_str": "2359474651",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QGaWrHqvs9",
+					"expanded_url": "https://www.amazon.de/ANSMANN-Timer-Steckdose-AES1-Energiespar-Steckdose/dp/B081FVXTTC/ref=sr_1_4?__mk_de_DE=",
+					"display_url": "amazon.de/ANSMANN-Timer-…",
+					"indices": [
+						53,
+						76
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1495112958107701200,
+		"in_reply_to_status_id_str": "1495112958107701248",
+		"in_reply_to_user_id": 617963,
+		"in_reply_to_user_id_str": "617963",
+		"in_reply_to_screen_name": "seiz",
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/5UFfaMMOCQ",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/5UFfaMMOCQ",
+							"expanded_url": "https://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 180,
+			"friends_count": 133,
+			"listed_count": 26,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 781,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16972,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "@techniktagebuch Ich darf noch nen Link nachliefern: https://t.co/QGaWrHqvs9ÅMÅŽÕÑ&amp;crid=3G32FDCGODGB9&amp;keywords=ansmann+steckdose+timer+1260&amp;qid=1645297782&amp;sprefix=ansmann+steckdosen+timer+1260%2Caps%2C98&amp;sr=8-4"
+	},
+	{
 		"created_at": "Thu Feb 17 21:09:33 +0000 2022",
 		"id": 1494418765471264800,
 		"id_str": "1494418765471264769",
