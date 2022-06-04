6ec40983d48688dbdff55419e4842f7dc6863f5d
Stefan Seiz <seiz@imd.net>
2022-06-04 09:14:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15722,
-	"created_at": "Tue May 31 07:14:19 +0000 2022"
+	"tweets": 15723,
+	"created_at": "Sat Jun 04 07:14:19 +0000 2022"
 }
---
+++ data/js/tweets/2022_06.js
@@ -0,0 +1,301 @@
+Grailbird.data.tweets_2022_06 = 
+[
+	{
+		"created_at": "Fri Jun 03 14:27:43 +0000 2022",
+		"id": 1532730754857422800,
+		"id_str": "1532730754857422849",
+		"full_text": "Wunderschön. https://t.co/b8BLOEJfo4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			12
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/b8BLOEJfo4",
+					"expanded_url": "https://twitter.com/peterglaser/status/1532728510195843073",
+					"display_url": "twitter.com/peterglaser/st…",
+					"indices": [
+						13,
+						36
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/5UFfaMMOCQ",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/5UFfaMMOCQ",
+							"expanded_url": "https://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 180,
+			"friends_count": 134,
+			"listed_count": 26,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 768,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17038,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1532728510195843000,
+		"quoted_status_id_str": "1532728510195843073",
+		"quoted_status_permalink": {
+			"url": "https://t.co/b8BLOEJfo4",
+			"expanded": "https://twitter.com/peterglaser/status/1532728510195843073",
+			"display": "twitter.com/peterglaser/st…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jun 03 14:18:48 +0000 2022",
+			"id": 1532728510195843000,
+			"id_str": "1532728510195843073",
+			"full_text": "• Olivetti Programma 101, der erste frei programmierbare Tischrechner der Welt (1965). Design: Mario Bellini https://t.co/OO1QYREsp8",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				108
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1532728460979880000,
+						"id_str": "1532728460979879936",
+						"indices": [
+							109,
+							132
+						],
+						"media_url": "http://pbs.twimg.com/media/FUVYah3WQAAWgA1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FUVYah3WQAAWgA1.jpg",
+						"url": "https://t.co/OO1QYREsp8",
+						"display_url": "pic.twitter.com/OO1QYREsp8",
+						"expanded_url": "https://twitter.com/peterglaser/status/1532728510195843073/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 418,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 787,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 738,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1532728460979880000,
+						"id_str": "1532728460979879936",
+						"indices": [
+							109,
+							132
+						],
+						"media_url": "http://pbs.twimg.com/media/FUVYah3WQAAWgA1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FUVYah3WQAAWgA1.jpg",
+						"url": "https://t.co/OO1QYREsp8",
+						"display_url": "pic.twitter.com/OO1QYREsp8",
+						"expanded_url": "https://twitter.com/peterglaser/status/1532728510195843073/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 418,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 787,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 738,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 67278209,
+				"id_str": "67278209",
+				"name": "Peter Glaser",
+				"screen_name": "peterglaser",
+				"location": "Berlin",
+				"description": "Ich schreibe.",
+				"url": "https://t.co/QhiMTnVSqE",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/QhiMTnVSqE",
+								"expanded_url": "https://www.nzz.ch/meinung/kolumnen/glaserei/",
+								"display_url": "nzz.ch/meinung/kolumn…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 11679,
+				"friends_count": 2471,
+				"listed_count": 491,
+				"created_at": "Thu Aug 20 10:06:30 +0000 2009",
+				"favourites_count": 8500,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 52899,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "023755",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/372004147/peterglaser_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/372004147/peterglaser_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/67278209/1397056064",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "AAA011",
+				"profile_sidebar_fill_color": "AAEEFF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6,
+			"favorite_count": 24,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "de"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Wunderschön. https://t.co/b8BLOEJfo4"
+	}
+]
