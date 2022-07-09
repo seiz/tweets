ed984a6d81a0c63b7e832530fe7b1c4c9890dc6b
Stefan Seiz <seiz@imd.net>
2022-07-09 09:14:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15740,
-	"created_at": "Fri Jul 08 07:14:19 +0000 2022"
+	"tweets": 15741,
+	"created_at": "Sat Jul 09 07:14:19 +0000 2022"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Fri Jul 08 17:03:28 +0000 2022",
+		"id": 1545385011251236900,
+		"id_str": "1545453528180744192",
+		"full_text": "♥ Amazing mini-site tells you what’s changed since the day you were born. Part of @nealagarwal’s https://t.co/CN2a9m34ky collection of brilliant mini-sites that exist simply to be and delight. (Remember those?) Hat tip: @kreativtek\n\nhttps://t.co/z4h2lbeWrx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			256
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nealagarwal",
+					"name": "Neal Agarwal",
+					"id": 376388111,
+					"id_str": "376388111",
+					"indices": [
+						82,
+						94
+					]
+				},
+				{
+					"screen_name": "kreativtek",
+					"name": "Jarosław Morawski",
+					"id": 866557956476866600,
+					"id_str": "866557956476866560",
+					"indices": [
+						220,
+						231
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/CN2a9m34ky",
+					"expanded_url": "http://Neal.Fun",
+					"display_url": "Neal.Fun",
+					"indices": [
+						97,
+						120
+					]
+				},
+				{
+					"url": "https://t.co/z4h2lbeWrx",
+					"expanded_url": "https://neal.fun/life-stats/",
+					"display_url": "neal.fun/life-stats/",
+					"indices": [
+						233,
+						256
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 61133,
+			"id_str": "61133",
+			"name": "zeldman",
+			"screen_name": "zeldman",
+			"location": "New York, NY",
+			"description": "Author. Designer. Web Standards Godfather. Employer Brand at @Automattic. Publisher, @AListApart, @ABookApart. Cofounder, @AnEventApart. Ava’s dad. He/him.",
+			"url": "https://t.co/hVWHiD8Sr7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hVWHiD8Sr7",
+							"expanded_url": "https://www.zeldman.com/",
+							"display_url": "zeldman.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 326315,
+			"friends_count": 5343,
+			"listed_count": 10572,
+			"created_at": "Tue Dec 12 17:11:41 +0000 2006",
+			"favourites_count": 80047,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 63101,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1540327628896456705/i0ypXD1o_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1540327628896456705/i0ypXD1o_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/61133/1656084296",
+			"profile_link_color": "CC3300",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FF6600",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 17,
+		"favorite_count": 57,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "♥ Amazing mini-site tells you what’s changed since the day you were born. Part of @nealagarwal’s https://t.co/CN2a9m34ky collection of brilliant mini-sites that exist simply to be and delight. (Remember those?) Hat tip: @kreativtek\n\nhttps://t.co/z4h2lbeWrx"
+	},
+	{
 		"created_at": "Thu Jul 07 14:36:04 +0000 2022",
 		"id": 1545054043638153200,
 		"id_str": "1545054043638153217",
