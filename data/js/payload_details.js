0e7c9c96e8acfb64eb1c995ba74e27ceff679522
Stefan Seiz <seiz@imd.net>
2022-08-10 09:14:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15754,
-	"created_at": "Sat Aug 06 07:14:19 +0000 2022"
+	"tweets": 15755,
+	"created_at": "Wed Aug 10 07:14:19 +0000 2022"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Tue Aug 09 16:05:16 +0000 2022",
+		"id": 1557035290073628700,
+		"id_str": "1557035290073628673",
+		"full_text": "@ZDF Danke! Ich hab das dann einfach live, also linear, wie so ein Steinzeitmensch angesehen.",
+		"truncated": false,
+		"display_text_range": [
+			5,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ZDF",
+					"name": "ZDF",
+					"id": 57350105,
+					"id_str": "57350105",
+					"indices": [
+						0,
+						4
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1556961525868503000,
+		"in_reply_to_status_id_str": "1556961525868503040",
+		"in_reply_to_user_id": 57350105,
+		"in_reply_to_user_id_str": "57350105",
+		"in_reply_to_screen_name": "ZDF",
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/5UFfaMMOCQ",
+			"url": "https://t.co/y45Sl34avS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y45Sl34avS",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/5UFfaMMOCQ",
+							"expanded_url": "https://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 177,
+			"friends_count": 132,
+			"listed_count": 0,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 787,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17084,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@ZDF Danke! Ich hab das dann einfach live, also linear, wie so ein Steinzeitmensch angesehen."
+	},
+	{
 		"created_at": "Fri Aug 05 09:04:56 +0000 2022",
 		"id": 1555479959749234700,
 		"id_str": "1555479959749234693",
