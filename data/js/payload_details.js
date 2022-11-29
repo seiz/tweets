3fe22bb669567862f56e9b16e6abfa08183a73f6
Stefan Seiz <seiz@imd.net>
2022-11-29 08:14:20 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15811,
-	"created_at": "Sat Nov 26 07:14:19 +0000 2022"
+	"tweets": 15812,
+	"created_at": "Tue Nov 29 07:14:19 +0000 2022"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Mon Nov 28 18:24:25 +0000 2022",
+		"id": 1597295362015912000,
+		"id_str": "1597295362015911936",
+		"full_text": "@markus_siepmann https://t.co/It5cxJG6Zd",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			16
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "markus_siepmann",
+					"name": "Markus Siepmann",
+					"id": 8783762,
+					"id_str": "8783762",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1597295357704249300,
+					"id_str": "1597295357704249345",
+					"indices": [
+						17,
+						40
+					],
+					"media_url": "http://pbs.twimg.com/media/Fiq7qj1XoAE8yA3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fiq7qj1XoAE8yA3.jpg",
+					"url": "https://t.co/It5cxJG6Zd",
+					"display_url": "pic.twitter.com/It5cxJG6Zd",
+					"expanded_url": "https://twitter.com/seiz/status/1597295362015911936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1125,
+							"h": 507,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 507,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 306,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1597295357704249300,
+					"id_str": "1597295357704249345",
+					"indices": [
+						17,
+						40
+					],
+					"media_url": "http://pbs.twimg.com/media/Fiq7qj1XoAE8yA3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fiq7qj1XoAE8yA3.jpg",
+					"url": "https://t.co/It5cxJG6Zd",
+					"display_url": "pic.twitter.com/It5cxJG6Zd",
+					"expanded_url": "https://twitter.com/seiz/status/1597295362015911936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1125,
+							"h": 507,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 507,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 306,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1597285431733026800,
+		"in_reply_to_status_id_str": "1597285431733026817",
+		"in_reply_to_user_id": 8783762,
+		"in_reply_to_user_id_str": "8783762",
+		"in_reply_to_screen_name": "markus_siepmann",
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/1v5DT4jOjh\n\nAlso on: @seiz@mastodon.social https://t.co/wftlIZsQfJ",
+			"url": "https://t.co/wiNnkarbD9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wiNnkarbD9",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/1v5DT4jOjh",
+							"expanded_url": "http://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						},
+						{
+							"url": "https://t.co/wftlIZsQfJ",
+							"expanded_url": "https://mastodon.social/@seiz",
+							"display_url": "mastodon.social/@seiz",
+							"indices": [
+								91,
+								114
+							]
+						}
+					]
+				}
+			},
+			"protected": true,
+			"followers_count": 171,
+			"friends_count": 77,
+			"listed_count": 25,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 785,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17147,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "qme",
+		"text": "@markus_siepmann https://t.co/It5cxJG6Zd"
+	},
+	{
 		"created_at": "Fri Nov 25 09:44:48 +0000 2022",
 		"id": 1596077434541072400,
 		"id_str": "1596077434541072385",
