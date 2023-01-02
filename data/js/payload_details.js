fe7920bfd985a11e8592c422670544abe431f60b
Stefan Seiz <seiz@imd.net>
2023-01-02 08:14:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15813,
-	"created_at": "Sat Dec 17 07:14:19 +0000 2022"
+	"tweets": 15814,
+	"created_at": "Mon Jan 02 07:14:19 +0000 2023"
 }
---
+++ data/js/tweets/2023_01.js
@@ -0,0 +1,129 @@
+Grailbird.data.tweets_2023_01 = 
+[
+	{
+		"created_at": "Sun Jan 01 16:59:36 +0000 2023",
+		"id": 1609595205547688000,
+		"id_str": "1609595205547687936",
+		"full_text": "@tim_cook May 2023 be a year of refocusing on quality software instead of pestering customers with subscription ads.",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			116
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tim_cook",
+					"name": "Tim Cook",
+					"id": 1636590253,
+					"id_str": "1636590253",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1609580804610592800,
+		"in_reply_to_status_id_str": "1609580804610592768",
+		"in_reply_to_user_id": 1636590253,
+		"in_reply_to_user_id_str": "1636590253",
+		"in_reply_to_screen_name": "tim_cook",
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/1v5DT4jOjh\n\nAlso on: @seiz@mastodon.social https://t.co/wftlIZsQfJ",
+			"url": "https://t.co/wiNnkarbD9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wiNnkarbD9",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/1v5DT4jOjh",
+							"expanded_url": "http://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						},
+						{
+							"url": "https://t.co/wftlIZsQfJ",
+							"expanded_url": "https://mastodon.social/@seiz",
+							"display_url": "mastodon.social/@seiz",
+							"indices": [
+								91,
+								114
+							]
+						}
+					]
+				}
+			},
+			"protected": true,
+			"followers_count": 170,
+			"friends_count": 75,
+			"listed_count": 25,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 777,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17147,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@tim_cook May 2023 be a year of refocusing on quality software instead of pestering customers with subscription ads."
+	}
+]
