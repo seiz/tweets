aa9cdc06cf28a7907fe577df94f5dee31630a436
Stefan Seiz <seiz@imd.net>
2023-05-03 09:14:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 15814,
-	"created_at": "Mon Jan 02 07:14:19 +0000 2023"
+	"tweets": 15815,
+	"created_at": "Wed May 03 07:14:19 +0000 2023"
 }
---
+++ data/js/tweets/2023_05.js
@@ -0,0 +1,129 @@
+Grailbird.data.tweets_2023_05 = 
+[
+	{
+		"created_at": "Wed May 03 06:33:17 +0000 2023",
+		"id": 1653648908034138000,
+		"id_str": "1653648908034138115",
+		"full_text": "@witt_software Hi, is Rocket Typist still supported? \nI ask as I just purchased a license and contacted Support via the Web-Form but did not receive any response.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			162
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "witt_software",
+					"name": "Daniel Witt 🇺🇦",
+					"id": 762180048631631900,
+					"id_str": "762180048631631872",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 762180048631631900,
+		"in_reply_to_user_id_str": "762180048631631872",
+		"in_reply_to_screen_name": "witt_software",
+		"user": {
+			"id": 617963,
+			"id_str": "617963",
+			"name": "zıəs uɐɟəʇs",
+			"screen_name": "seiz",
+			"location": "48.70516,9.651704",
+			"description": "Designer. \nPresumably still alive.\nhttps://t.co/1v5DT4jOjh\n\nAlso on: @seiz@mastodon.social https://t.co/wftlIZsQfJ",
+			"url": "https://t.co/wiNnkarbD9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wiNnkarbD9",
+							"expanded_url": "http://meckr.imd.net",
+							"display_url": "meckr.imd.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/1v5DT4jOjh",
+							"expanded_url": "http://stefanseiz.com",
+							"display_url": "stefanseiz.com",
+							"indices": [
+								35,
+								58
+							]
+						},
+						{
+							"url": "https://t.co/wftlIZsQfJ",
+							"expanded_url": "https://mastodon.social/@seiz",
+							"display_url": "mastodon.social/@seiz",
+							"indices": [
+								91,
+								114
+							]
+						}
+					]
+				}
+			},
+			"protected": true,
+			"followers_count": 168,
+			"friends_count": 74,
+			"listed_count": 22,
+			"created_at": "Tue Jan 09 13:46:12 +0000 2007",
+			"favourites_count": 765,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17143,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/956583216860811264/BRSgKHuf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/617963/1437646424",
+			"profile_link_color": "333333",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "2E2E2E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@witt_software Hi, is Rocket Typist still supported? \nI ask as I just purchased a license and contacted Support via the Web-Form but did not receive any response."
+	}
+]
